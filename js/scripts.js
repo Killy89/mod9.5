5b8f8211cdd572dfbcec855504b59e22f3cc6dd7
Michał Marciniak <marciniak.michal89@gmail.com>
2017-12-01 17:25:42 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var itemButton = document.getElementsByClassName('button');
+
+for (var i = 0; i < itemButton.length; i++) {
+    var nameButton = itemButton[i].innerText;
+    var text = nameButton;
+    console.log(text);
+}
